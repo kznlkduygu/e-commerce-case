cd2627c72bb072dddb0875f5847cc90c83ae0755
duygu.kizanlik <duygu.kizanlik@bulutmd.com>
2024-05-07 22:43:35 +0300

    tailwind css added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "blue-600": "#2A59FE",
+      },
+    },
+  },
+  plugins: [],
+};
