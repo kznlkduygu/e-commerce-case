e27fd6908c0e21c133f7cade84af5d9ef66be784
duygu.kizanlik <duygu.kizanlik@bulutmd.com>
2024-05-11 00:42:44 +0300

    sortData added

---
+++ src/utils/sortData.ts
@@ -0,0 +1,7 @@
+export const sortData = [
+          { value: "dateAsc", label: "Old to new" },
+          { value: "dateDesc", label: "New to old" },
+          { value: "priceDesc", label: "Price high to low" },
+          { value: "priceAsc", label: "Price low to high" },
+        ];
+        
