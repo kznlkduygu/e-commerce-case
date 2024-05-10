e19ecb3f91e24402036af79173d840651db1bb26
duygu.kizanlik <duygu.kizanlik@bulutmd.com>
2024-05-11 00:13:34 +0300

    added page routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./index.css";
-import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import "./sass/layout.scss";
+import "./sass/header.scss";
+import "./sass/card.scss";
+import "./sass/sort.scss";
+import "./sass/filter.scss";
+import "./sass/basketCard.scss";
+import "./sass/checkoutCard.scss";
+import "./sass/list.scss";
+import "./sass/detail.scss";
+
+import List from "./components/List";
+import ProductList  from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}>
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <ProductList>
+                <Outlet />
+              </ProductList>
+            }
+          >
+            <Route index element={<Navigate to={"productList"} />} />
+            <Route path={"productList"} element={<List />} />
+            <Route path={"productDetail"} element={<Outlet />}>
+              <Route path={":id"} element={<ProductDetail/>} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
